3e98a6dcba6313fd0260095bf600449cf931a983
Jhon Jairo Gelvez Gomez <jhonjairogelvezgomez@gmail.com>
2026-03-12 09:58:32 -0500

    archivo html, js para el ejercicio 5. estilos para todos los archivos

---
+++ 5.js
@@ -0,0 +1,20 @@
+// 5. Sistema de calificaciones con condiciones lógicas
+// Un estudiante aprueba una materia si:
+// • Su nota final es mayor o igual a 60 y
+// • Su asistencia es mayor o igual al 80%.
+// Si no cumple ambas condiciones, debe mostrar "Reprobado".
+// Pregunta de análisis: ¿Qué operador lógico garantiza que ambas condiciones se
+// evalúen al mismo tiempo?
+
+// Solicitar la nota al usuario
+let nota = parseInt(prompt("Ingrese la nota final:"));
+
+// Solicitar el porcentaje de asistencia
+let asistencia = parseInt(prompt("Ingrese el porcentaje de asistencia:"));
+
+// Verificar ambas condiciones
+if (nota >= 60 && asistencia >= 80) {
+    alert("Aprobado");
+} else {
+    alert("Reprobado");
+}
